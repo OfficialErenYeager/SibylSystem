baf2d851401313aaeb179e28d6945e34639e6ff5
Eren Yeager <78803287+OfficialErenYeager@users.noreply.github.com>
2021-04-04 01:58:04 +0530

    Update sample_config.py

--- Sibyl_System/sample_config.py
+++ Sibyl_System/sample_config.py
@@ -1,11 +1,11 @@
 from typing import Union, Optional
 
-API_ID: int = 12345
-API_HASH: str = "XYZ"
-STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Sibyl_logs: Union[str, int] = 123456789
-Sibyl_approved_logs: Union[str, int] = 123456789
-GBAN_MSG_LOGS: Optional[Union[str, int]] = None
-BOT_TOKEN: str = "123456:shsiajskzkxhxj"
+API_ID: int = 3296233
+API_HASH: str = "b6315a1fc6d4d15d5278fea0c5a99c04"
+STRING_SESSION: str = "1BVtsOHcBu4oSCS5_Vfbnx8hR-eKBagqNuKIvGxVZCI4_f9KWQoFMOX47J7ZdkFgriwPQ8woPPCt83yPOxexGQdNNFu5bvl4OWTbPGl6MQgu739BD5MSGUs0Fc_LBwbWdhB4r7t7DhTyrLfqoU1hgBekEhF1ywqWs6KCwjiV5BliuHDp0UCMgbLi6s7PgNAM1w87WrNrfehhuXpImbIeD4ELhEXFqCJ7ix7KM7HUOX87ZXRVMGUatPVx3oCcYnxZPZnEXjm33TNKBYsHXvgwe2fQ_zmLKt6-qFAn0fLOUlA68T4lQksd7XZSyvN3DC5KXJCQy8-LhNGZE9NDIQfyGXK2JNZ9TNG8="
+Sibyl_logs: Union[str, int] = -569492198
+Sibyl_approved_logs: Union[str, int] = -562922694
+GBAN_MSG_LOGS: Optional[Union[str, int]] = -508756914
+BOT_TOKEN: str = "1752238934:AAH8l3fpkzx6Z-r4r5qRcKwlGjbCo5RBPfA"
 
 # Remove this line one you have read and understood the explanation of things in https://github.com/AnimeKaizoku/SibylSystem/blob/master/README.md
